758c8934aeb8ba15023bc10ab7aa439b0147b6ba
Hiro Tech <odamatsu@gmail.com>
2018-11-10 23:29:47 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/login" => "users#login_form"
+  post "/login" => "users#login"
+  post "/logout" => "users#logout"
+
+  get 'users/index' => "users#index"
+  get "users/signup" => "users#new"
+  post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
+  get "users/:id" => "users#show"
+
+  get 'posts/index' => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+
+  get "/" => "home#top"
+  get "about" => "home#about"
+
 end
